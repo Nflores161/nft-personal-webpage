8db6ffa15f13d1b7559079fd6807280c348c9aa6
Nicholas Flores-Tober <nflores161@gmail.com>
2021-03-28 01:29:32 -0400

    Added JS mouseover event to home area

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
 
-const heady = document.getElementById('header');
+const home = document.getElementById('home');
 
-heady.style.color = "#2baec5";
+home.addEventListener("mouseover", mouseDisplay);
+
+function mouseDisplay() {
+  console.log("mouse display worked");
+  home.style.color = "#2baec5";
+}
